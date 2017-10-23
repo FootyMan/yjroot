f4e280e87cb4155dd7fc2c477a04d07b5f38fe0a
DESKTOP-0FE4V6D\HCY <haogj3006@126.com>
2017-10-23 23:17:48 +0800

    no message

---
+++ yjerp/src/main/java/com/erp/model/ProvinceModel.java
@@ -0,0 +1,19 @@
+package com.erp.model;
+
+public class ProvinceModel {
+
+	private int provinceId;
+	private String name;
+	public int getProvinceId() {
+		return provinceId;
+	}
+	public void setProvinceId(int provinceId) {
+		this.provinceId = provinceId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
